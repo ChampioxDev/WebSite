821a17b5cb0fb075a0e9d267e671f46377b74ad1
ChampioxDev <max.e.goenuel@gmail.com>
2023-05-17 11:07:36 +0200

    code refactor + dark mode

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+var lightMode = "True";
 function load() {
     // ageCalc
     var datum = new Date();
     document.getElementById("alter").innerHTML = datum.getFullYear()-2007;
+}
+function toggleLightMode() {
+    if (lightMode == "True") {
+        document.getElementById("body").style.backgroundColor = "#333333";
+        document.getElementById("aboutMe").style.backgroundColor = "#555555";
+        document.getElementById("aboutMe").style.color = "#FFFFFF";
+        document.getElementById("myProj").style.backgroundColor = "#555555";
+        document.getElementById("myProj").style.color = "#FFFFFF";
+        document.getElementById("contact").style.backgroundColor = "#555555";
+        document.getElementById("contact").style.color = "#FFFFFF";
+        lightMode = "False";
+    }
+    else {
+        document.getElementById("body").style.backgroundColor = "#FFFFFF";
+        document.getElementById("aboutMe").style.backgroundColor = "#cccccc";
+        document.getElementById("aboutMe").style.color = "#000000";
+        document.getElementById("myProj").style.backgroundColor = "#cccccc";
+        document.getElementById("myProj").style.color = "#000000";
+        document.getElementById("contact").style.backgroundColor = "#cccccc";
+        document.getElementById("contact").style.color = "#000000";
+        lightMode = "True";
+    }
 }
